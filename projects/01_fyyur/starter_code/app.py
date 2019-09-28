99ef3d53140e7b5b321887823dede2bd2db27fec
Sepehr <sepehr.ramyar@gmail.com>
2019-09-28 12:27:59 -0700

    add venue seeking artist column and artist seeking venue column, migrate to new db schema

--- projects/01_fyyur/starter_code/app.py
+++ projects/01_fyyur/starter_code/app.py
@@ -42,6 +42,7 @@ class Venue(db.Model):
     phone = db.Column(db.String(120))
     image_link = db.Column(db.String(500))
     facebook_link = db.Column(db.String(120))
+    seeking_talent = db.Column(db.Boolean, nullable = False)
 
     # TODO: implement any missing fields, as a database migration using Flask-Migrate
 
@@ -56,6 +57,7 @@ class Artist(db.Model):
     genres = db.Column(db.String(120))
     image_link = db.Column(db.String(500))
     facebook_link = db.Column(db.String(120))
+    seeking_venue = db.Column(db.Boolean, nullable = False)
 
     # TODO: implement any missing fields, as a database migration using Flask-Migrate
 
